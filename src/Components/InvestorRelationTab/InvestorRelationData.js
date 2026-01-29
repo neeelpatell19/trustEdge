0d7b568a2b9c567b4c64f2caf44ee3e13df35fc6
Abhi solanki <abhi85076@gmail.com>
2026-01-29 10:20:33 +0530

    fixed: pdf uploaded

--- src/Components/InvestorRelationTab/InvestorRelationData.js
+++ src/Components/InvestorRelationTab/InvestorRelationData.js
@@ -404,6 +404,10 @@ const investorRelationData = [
                 name: "30.09.2025",
                 path: "/Images/Icons/Documents/Financial Result/Quarterly Financial Result/2025-26/30.09.2025.pdf",
               },
+              {
+                name: '30.12.2025',
+                path: "/Images/Icons/Documents/Financial Result/Quarterly Financial Result/2025-26/30.12.2025.pdf"
+              }
             ],
           },
           {
